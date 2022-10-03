00ee0a25183ec25838761f813136a0b06f1f07c3
arfan36 <arfanur36@gmail.com>
2022-10-03 19:15:44 +0600

    51-2 Install React Bootstrap in your react application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { Button, Spinner } from 'react-bootstrap';
 import './App.css';
+import GridCard from './components/GridCard/GridCard';
+import NavBar from './NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar></NavBar>
+      <button>Plain Button</button>
+      <Button variant='danger'>My Button</Button>
+      <Spinner animation='grow' variant='danger' />
+      <GridCard></GridCard>
     </div>
   );
 }
