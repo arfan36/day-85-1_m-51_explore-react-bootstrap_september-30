05ce0590e4c92ad36b3c469d54b3669eb6c9e7e1
arfan36 <arfanur36@gmail.com>
2022-10-03 19:52:27 +0600

    Toast added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Button, Spinner } from 'react-bootstrap';
 import './App.css';
 import GridCard from './components/GridCard/GridCard';
+import ToastTest from './components/ToastTest/ToastTest';
 import NavBar from './NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+      <ToastTest></ToastTest>
       <button>Plain Button</button>
       <Button variant='danger'>My Button</Button>
       <Spinner animation='grow' variant='danger' />
